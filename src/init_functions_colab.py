33977d80598aa76dc56bbff54c4b4440aad7f19d
0xRusowsky <0xrusowsky@gmail.com>
2022-04-26 07:41:57 +0200

    Add all historical data

--- src/init_functions_colab.py
+++ src/init_functions_colab.py
@@ -5,17 +5,13 @@ from typing import Dict, List, Tuple
 # Get initial params based on historical data
 def initial_params(netflow_type:str, initial_date:str=None, initial_supply:float=None, initial_reserves:float=None, initial_liq_usd:float=None, initial_price:float=None, initial_target:float=None):
     if netflow_type == 'historical' and initial_date is not None:
-        historical_df = pd.read_csv('/content/liquidity-olympus/data/historical_ohm_data.csv', usecols= ['date','net_flows', 'price', 'supply'])
+        historical_df = pd.read_csv('/content/liquidity-olympus/data/historical_ohm_data.csv', usecols= ['date','net_flows', 'price', 'supply','liquidity','reserves'])
         initial_index = historical_df[historical_df == initial_date]['date'].dropna().index[0]
         historical_net_flows = historical_df[historical_df.index >= initial_index]['net_flows'].tolist()
-        price, supply = historical_df.iloc[initial_index, [historical_df.columns.get_loc(c) for c in ['price', 'supply']]]
+        price, supply, liq_usd, reserves = historical_df.iloc[initial_index, [historical_df.columns.get_loc(c) for c in ['price', 'supply','liquidity','reserves']]]
     else:
         historical_net_flows = None
-        price = initial_price
-        supply = initial_supply
 
     target = price
-    reserves = initial_reserves
-    liq_usd = initial_liq_usd
     
     return netflow_type, historical_net_flows, price, target, supply, reserves, liq_usd
