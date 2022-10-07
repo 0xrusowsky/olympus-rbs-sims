709fc9e1e1b428d64e3ca3f6c4be6ebed137ce8d
0xrusowsky <0xrusowsky@proton.me>
2022-10-07 11:35:10 +0200

    House keeping

--- simulation_variables_XX.py
+++ simulation_variables_XX.py
@@ -33,8 +33,8 @@ job_config_upload = bigquery.LoadJobConfig(
 # Simulate scenario with market operations
 def model_inputs (initial_variables, max_liq_ratio, ask_factor, cushion_factor, lower_wall, lower_cushion, mint_sync_premium, with_reinstate_window, with_dynamic_reward_rate, seed):
     netflow_type, historical_net_flows, price, target, supply, reserves, liq_usd = initial_params(
-         netflow_type = 'enforced' # determines the netflow types. Either 'historical', 'random', or 'cycles' (sin/cos waves)
-        ,netflow_data = '169' # determines the initial date to account for 'historical' netflows and initial params. (example: '2021/12/18')
+        netflow_type = 'random' # determines the netflow types. Either 'historical', 'random', or 'cycles' (sin/cos waves)
+        #,initial_date = '2021/12/18' # Determines the initial date to account for 'historical' netflows and initial params. (example: '2021/12/18')
         ,initial_supply = initial_variables[0]
         ,initial_reserves = initial_variables[1]
         ,initial_liq_usd = initial_variables[2]
@@ -61,7 +61,7 @@ def model_inputs (initial_variables, max_liq_ratio, ask_factor, cushion_factor, 
 
         ,max_liq_ratio = max_liq_ratio  # liquidityUSD : reservesUSD ratio --> 1:1 = 0.5
         ,min_premium_target = mint_sync_premium  # minimum premium to keep adding liquidity as supply grows (mint & sync).
-        ,max_outflow_rate = 0.05 # max % of reservesUSD that can be released on a single day
+        ,max_outflow_rate = 0.033 # max % of reservesUSD that can be released on a single week
         ,reserve_change_speed = 1  # directly related to the speed at which reserves are released/captured by the treasury. The higher the slower.
         ,with_reinstate_window = with_reinstate_window # determines if there is a minimum counter to reinstate the capacity to perform operations or not
         ,with_dynamic_reward_rate = with_dynamic_reward_rate # determines if there is less supply expansion when price < wall
@@ -94,7 +94,7 @@ def model_inputs (initial_variables, max_liq_ratio, ask_factor, cushion_factor, 
     else:
         simulation = {'day1': Day(params=params, prev_arbs=arbs, prev_lags=lags, historical_net_flows=historical_net_flows[0])}
         for i in range (2, min(params.horizon, len(historical_net_flows) - 1)):
-            simulation[f'day{i}'] = Day(params=params, prev_arbs=arbs, prev_lags=lags, prev_day=simulation[f'day{i-1}'], historical_net_flows=historical_net_flows[i-1])
+            simulation[f'day{i}'] = Day(params=params, prev_arbs=arbs, prev_lags=lags, prev_day=simulation[f'day{i-1}'], historical_net_flows=historical_net_flows[i-2])
 
     return simulation
 
@@ -104,62 +104,58 @@ def get_trial_variables(from_df, initial_variables):
         
     for key, value in from_df.iterrows():
         simulation = model_inputs(seed = value['seed']
-                                  ,max_liq_ratio = value['maxLiqRatio']
-                                  ,ask_factor = value['askFactor']
-                                  ,cushion_factor = value['cushionFactor']
-                                  ,lower_wall = value['wall']
-                                  ,lower_cushion = value['cushion']
-                                  ,mint_sync_premium = value['mintSyncPremium']
-                                  ,with_reinstate_window = value['withReinstateWindow']
-                                  ,with_dynamic_reward_rate = value['withDynamicRR']
+                                  , max_liq_ratio = value['maxLiqRatio']
+                                  , ask_factor = value['askFactor']
+                                  , cushion_factor = value['cushionFactor']
+                                  , lower_wall = value['wall']
+                                  , lower_cushion = value['cushion']
+                                  , mint_sync_premium = value['mintSyncPremium']
+                                  , with_reinstate_window = value['withReinstateWindow']
+                                  , with_dynamic_reward_rate = value['withDynamicRR']
                                   ,initial_variables = initial_variables
                                   )
 
         for day, data in simulation.items():
             result_df.loc[len(result_df)] = [str(f'{value["key"]}'), float(data.day), float(data.net_flow), float(data.price), float(data.ma_target), float(data.lower_target_cushion), float(data.upper_target_cushion), float(data.lower_target_wall), float(data.upper_target_wall), float(data.liq_usd), float(data.liq_ohm), float(data.k), float(data.reserves), float(100*data.reserves/data.prev_reserves), float(data.reserves_in), float(data.reserves_out), float(data.ohm_traded), float(data.treasury), float(data.supply), float(data.mcap), float(data.floating_supply), float(data.floating_mcap), float(data.liq_ratio), float(data.liq_usd/data.reserves), float(data.reserves_ratio), float(data.liq_fmcap_ratio), float(data.fmcap_treasury_ratio), float(data.cum_ohm_purchased), float(data.cum_ohm_burnt), float(data.bid_capacity), float(data.ask_capacity), float(data.bid_capacity_cushion), float(data.ask_capacity_cushion), float(data.bid_capacity_target_cushion), float(data.ask_capacity_target_cushion), float(data.bid_capacity_target), float(data.ask_capacity_target), float(data.control_ask), float(data.control_bid), float(data.market_demand), float(data.market_supply), float(data.total_net), float(data.gohm_volatility)]
 
-        print(value['seed'])
-
     return result_df
 
 
-# Load data from BigQuery: 169, 
-for t in [169]: #[169, 135, 213, 679, 2864, 4626, 47, 107, 124, 3295, 405, 340]:
-    #------------
-    #seeds 0-1000
-    query = f"""select * from `{read_table_id}` where seed = @seed"""
+# Load data from BigQuery
+for s in range (0, 60):
+    query = query = f"""select * from `{read_table_id}` where seed = @seed order by key asc LIMIT 1000"""
     job_config = bigquery.QueryJobConfig(
         query_parameters=[
-            bigquery.ScalarQueryParameter("seed", "INT64", t),
+            bigquery.ScalarQueryParameter("seed", "INT64", s),
             ]
         )
     parameters_df = (
         client.query(query, job_config).result().to_dataframe(create_bqstorage_client=True)
     )
 
-    print(f"Current config: {t}")
-    print(f"config {t} status | START Printing data pulled from BigQuery")
+    print(f"Current seed: {s}")
+    print(f"seed {s} status | START Printing data pulled from BigQuery")
     print(parameters_df)
-    print(f"config {t} status | END Printing data pulled from BigQuery")
+    print(f"seed {s} status | END Printing data pulled from BigQuery")
 
     # Get all the historical data from the simulated scenario
-    print(f"config {t} status | START Re-simulating data for all trials")
+    print(f"seed {s} status | START Re-simulating data for all trials")
     historical_df = get_trial_variables(parameters_df, initial_variables)
-    print(f"config {t} status | END Re-simulating data for all trials")
+    print(f"seed {s} status | END Re-simulating data for all trials")
     print(historical_df)
 
     # Load updated data
-    print(f"config {t} status | START uploading data into BigQuery")
+    print(f"seed {s} status | START uploading data into BigQuery")
     job = client.load_table_from_dataframe(
         historical_df, table_id, job_config=job_config_upload, location="US"
     )
     job.result()
-    print(f"config {t} status | END uploading data into BigQuery")
+    print(f"seed {s} status | END uploading data into BigQuery")
 
     # Print out confirmed job details
     table = client.get_table(table_id)
     print(
-        "config status | Loaded {} rows and {} columns to {}".format(
+        "seed status | Loaded {} rows and {} columns to {}".format(
             table.num_rows, len(table.schema), table_id
         )
     )
